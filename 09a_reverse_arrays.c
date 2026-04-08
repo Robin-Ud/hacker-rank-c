65a11c81ee1397806ac797122cd5379d11ecf98f
RobinUd <matheusprestes2006@gmail.com>
2026-04-08 19:23:55 -0300

    reverse array v2

--- 09a_reverse_arrays.c
+++ 09a_reverse_arrays.c
@@ -6,20 +6,25 @@ int main() {
   int num, *arr, i;
   scanf("%d", &num);
   arr = (int *)malloc(num * sizeof(int));
+
   for (i = 0; i < num; i++) {
     scanf("%d", arr + i);
   }
 
   /* inicio da minha logica */
-  int *rarray = (int *)malloc(sizeof(int) * num);
-  for (i = 0; i < num; i++) {
-    rarray[num - 1 - (i)] = arr[i];
+  int tmp = 0, rig = (num - 1), lef = 0;
+  while (lef < rig) {
+    tmp = arr[lef];
+    arr[lef] = arr[rig];
+    arr[rig] = tmp;
+    rig--;
+    lef++;
   }
   /* fim da minha logica    */
 
-  for (i = 0; i < num; i++)
+  for (i = 0; i < num; i++) {
     printf("%d ", *(arr + i));
+  }
   free(arr);
-  free(rarray);
   return 0;
 }
